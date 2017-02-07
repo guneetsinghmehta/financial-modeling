bcd31362ad05cd8b66bafcdee5f22debbbbd0940
guneetsinghmehta <mehta.guneet@gmail.com>
2017-02-07 13:41:53 -0600

    getCompanyDataYahoo gets data from yahoo API - no API limit

---
+++ Data_Collection_Scripts/getCompanyDataYahoo.py
@@ -0,0 +1,36 @@
+'''
+Created on Feb 7, 2017
+
+@author: zeus
+'''
+#https://pypi.python.org/pypi/yahoo-finance
+from yahoo_finance import Share
+import time
+import os
+import json
+
+def getCompanyDataYahoo(ticker):
+    start_date='2007-1-1'
+    end_date='2017-1-1'
+    newpath = '../Data/yahoo_data/' + ticker
+    try:                
+        company=Share(ticker)
+        historical_data=company.get_historical(start_date,end_date)
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+        fileHandle = open(newpath + '/data.json', 'wb')
+        json.dump(historical_data, fileHandle)
+        fileHandle.close()
+        print ticker+" found"
+    except Exception as error:
+        print  ticker+" NOT found"
+        print  error
+    
+def runAll():
+    fileHandle=open('tickerList2.txt','r')
+    for line in fileHandle:
+        ticker=line.strip()
+        getCompanyDataYahoo(ticker)
+        #print(ticker)
+runAll()
+   
